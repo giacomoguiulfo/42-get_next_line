c9c41895dc3f258cd1b320fd882f26d5068e2cd9
gguiulfo <gguiulfo@student.42.us.org>
2017-06-23 05:43:32 -0700

    New get_next_line

--- get_next_line.h
+++ get_next_line.h
@@ -3,25 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gguiulfo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 21:45:24 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/03/23 17:41:38 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/06/23 05:43:06 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <sys/types.h>
-# include <sys/uio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <sys/uio.h>
+# include <sys/types.h>
 # include "libft/libft.h"
+# define BUFF_SIZE 1024
 
-# define BUFF_SIZE 1
-# define GNL_MAX_FD 4864
+typedef struct	s_gnl
+{
+	int			fd;
+	char		*file_content;
+}				t_gnl;
 
-int		get_next_line(const int fd, char **line);
+int				get_next_line(const int fd, char **line);
 
 #endif
