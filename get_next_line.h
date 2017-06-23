f5c8773c36c4243d587b6003b562a59bdcdb278b
gguiulfo <gguiulfo@student.42.us.org>
2017-06-23 05:52:09 -0700

    Cleaned merge

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 21:45:24 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/06/23 05:43:06 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/06/23 05:48:37 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,11 @@
 # include "libft/libft.h"
 # define BUFF_SIZE 1024
 
-<<<<<<< HEAD
 typedef struct	s_gnl
 {
 	int			fd;
 	char		*file_content;
 }				t_gnl;
-=======
-# define BUFF_SIZE 4096
->>>>>>> 93618e1fe34bfca77f01d7c9727615369dd80fd4
 
 int				get_next_line(const int fd, char **line);
 
