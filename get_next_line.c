6baca5027c1a50a2794d47a75b01bd326b76edbc
gguiulfo <gguiulfo@student.42.us.org>
2017-06-24 23:42:31 -0700

    New get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,13 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 21:44:34 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/06/24 11:07:01 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/06/24 23:39:49 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void		gnl_free(void *content, size_t size)
-{
-	(void)size;
-	ft_strdel(&((t_gnl *)content)->file_content);
-	free(content);
-}
-
-static int		gnl_read(int fd, char **line, char *content)
+static int	gnl_read(int fd, char **line, char *content)
 {
 	char	buf[BUFF_SIZE + 1];
 	char	*tmp;
@@ -29,52 +22,62 @@ static int		gnl_read(int fd, char **line, char *content)
 	content[0] = '\0';
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
-		if (ret < 0)
-			return (-1);
+		GNL_CHK(ret < 0);
 		buf[ret] = '\0';
 		if ((ptr = ft_strchr(buf, '\n')) != NULL)
-		{
 			ft_strcpy(content, ptr + 1);
+		if (ptr != NULL)
 			*ptr = '\0';
-		}
 		tmp = *line;
-		if ((*line = ft_strjoin(*line, buf)) == NULL)
-			return (-1);
-		free(tmp);
+		GNL_CHK((*line = ft_strjoin(*line, buf)) == NULL);
+		ft_strdel(&tmp);
 		if (ptr)
 			return (1);
 	}
-	return ((*line[0] != '\0') ? 1 : 0);
+	if (*line[0] == '\0')
+		ft_strdel(line);
+	else
+		return (1);
+	return (0);
 }
 
-static t_list	*gnl_new_fd(t_list **begin_list, int fd)
-{
-	t_gnl	new;
-
-	new.fd = fd;
-	new.file_content = ft_strnew(BUFF_SIZE);
-	ft_lstadd(begin_list, ft_lstnew(&new, sizeof(t_gnl)));
-	return (*begin_list);
-}
-
-static int		gnl_cmp_fd(t_gnl *node, int *fd)
+static int	gnl_cmp_fd(t_gnl *node, int *fd)
 {
 	return (node->fd - *fd);
 }
 
-int				get_next_line(const int fd, char **line)
+t_list		**ft_gnl_list(void)
 {
 	static t_list	*head;
-	t_list			*tmp;
-	char			*ptr;
-	int				ret;
 
-	if (fd < 0 || !line)
-		return (-1);
-	if ((tmp = ft_lstfind(head, (void *)&fd, gnl_cmp_fd)) == NULL)
-		tmp = gnl_new_fd(&head, fd);
-	if ((*line = ft_strdup(((t_gnl*)tmp->content)->file_content)) == NULL)
-		return (-1);
+	return (&head);
+}
+
+void		ft_gnl_free(void *content, size_t size)
+{
+	(void)size;
+	ft_strdel(&((t_gnl *)content)->file_content);
+	free(content);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	t_list	**head;
+	t_list	*tmp;
+	t_gnl	new;
+	char	*ptr;
+	int		ret;
+
+	GNL_CHK(fd < 0 || !line);
+	head = ft_gnl_list();
+	if ((tmp = ft_lstfind(*head, (void *)&fd, gnl_cmp_fd)) == NULL)
+	{
+		new.fd = fd;
+		new.file_content = ft_strnew(BUFF_SIZE);
+		ft_lstadd(head, ft_lstnew(&new, sizeof(t_gnl)));
+		tmp = *head;
+	}
+	GNL_CHK((*line = ft_strdup(((t_gnl*)tmp->content)->file_content)) == NULL);
 	if ((ptr = ft_strchr(*line, '\n')) != NULL)
 	{
 		ft_strcpy((((t_gnl *)tmp->content)->file_content), ptr + 1);
@@ -82,9 +85,6 @@ int				get_next_line(const int fd, char **line)
 		return (1);
 	}
 	if ((ret = gnl_read(fd, line, (((t_gnl*)tmp->content)->file_content))) == 0)
-	{
-		ft_strdel(line);
-		ft_lstdelnode(&head, tmp, gnl_free);
-	}
+		ft_lstdelnode(head, tmp, ft_gnl_free);
 	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,21 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 21:45:24 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/06/24 11:03:33 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/06/24 23:41:35 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/uio.h>
 # include <sys/types.h>
 # include "libft/libft.h"
+
 # define BUFF_SIZE 1024
+# define GNL_CHK(a) if(1){if(a){return(-1);}}
 
 typedef struct	s_gnl
 {
@@ -26,5 +29,7 @@ typedef struct	s_gnl
 }				t_gnl;
 
 int				get_next_line(const int fd, char **line);
+void			ft_gnl_free(void *content, size_t size);
+t_list			**ft_gnl_list(void);
 
 #endif
